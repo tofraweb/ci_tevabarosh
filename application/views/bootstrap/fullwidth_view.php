0ba8ff0ef95cfa19e34dd33cfea898cdc038e1eb
tofra <tofraweb@gmail.com>
2017-05-21 12:41:34 +0300

    basic upload

--- application/views/bootstrap/fullwidth_view.php
+++ application/views/bootstrap/fullwidth_view.php
@@ -21,6 +21,26 @@
         <div class="row">
             <div class="col-lg-12">
                 <p>Most of Start Bootstrap's unstyled templates can be directly integrated into the Modern Business template. You can view all of our unstyled templates on our website at <a href="http://startbootstrap.com/template-categories/unstyled">http://startbootstrap.com/template-categories/unstyled</a>.</p>
+
+                <!-- image uploader -->
+                <form action="<?php base_url() ?>index.php/upload/do_upload" method="POST" enctype="multipart/form-data" >
+                Select File To Upload:<br />
+                <input type="file" name="userfile" multiple="multiple"  />
+
+                <input type="submit" name="submit" value="Upload" class="btn btn-success" />
+                  </form>
+
+                  <?php /*
+                  if isset($uploaded_file) {
+                      foreach ($uploaded_file as $name => $value){
+                          echo $name->value."<br />";
+                      }
+                    }*/
+                  ?>
+
+
+  <!-- image uploader end -->
+
             </div>
         </div>
         <!-- /.row -->
--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -112,6 +112,9 @@
                     <li>
                         <a href="<?php echo base_url();?>index.php/page/get_name/contact">צור קשר</a>
                     </li>
+                    <li>
+                        <a href="<?php echo base_url();?>index.php/upload">הוסף צמח</a>
+                    </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
