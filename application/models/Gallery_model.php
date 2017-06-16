5ae746fac3d0412dc49ba6784cfa53dda12b1cf6
tofra <tofraweb@gmail.com>
2017-06-16 13:24:09 +0300

    show 3 first images as thumbnails in species info tab - 2

---
+++ application/models/Gallery_model.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Gallery_model extends CI_Model{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function FunctionName($sp_id)
+	{
+		# code...
+	}
+
+}
